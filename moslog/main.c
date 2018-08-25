4d99da5f94a676a66d70baa6b00b0c001915361a
Yoann Pigné <yoann.pigne@gmail.com>
2018-08-25 20:11:51 +0200

    Initial Commit

---
+++ moslog/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  moslog
+//
+//  Created by Yoann Pigné on 25/08/2018.
+//  Copyright © 2018 Yoann Pigné. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
